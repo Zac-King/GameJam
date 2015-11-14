ce050509640e120d45a0ff20f54c27575397ecfb
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-11-14 14:44:55 -0600

    Project Fixing

--- IDGA GameJam/Assets/Scripts/CharacterController.cs
+++ IDGA GameJam/Assets/Scripts/CharacterController.cs
@@ -48,7 +48,7 @@ public class CharacterController : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		field = Instantiate(gravityField) as GameObject;
+		field = Instantiate(gravityField, transform.position, Quaternion.identity) as GameObject;
 		source = GameObject.FindWithTag("Player");
 	}
 
