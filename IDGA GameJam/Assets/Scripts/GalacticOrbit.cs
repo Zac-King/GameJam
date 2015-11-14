49b0c4add29b36ac2e592076ce042125852545a4
Zac-King <Zacharyking18@yahoo.com>
2015-11-13 23:20:01 -0600

    git push

---
+++ IDGA GameJam/Assets/Scripts/GalacticOrbit.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GalacticOrbit : MonoBehaviour
+{
+    [SerializeField]
+    private float rangeOfInfluence = 0.5f;
+    [SerializeField]
+    private float bodyMass = 0;
+    [SerializeField]
+    private float speed = 100;
+    [SerializeField]
+    private List<GameObject> BodiesOfInfluence = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log("hit");
+        gameObject.GetComponent<SphereCollider>().radius = rangeOfInfluence;
+    }
+
+    void OnTriggerEnter(Collider go)
+    {
+        //Debug.Log("TriggerEnter Hit");
+        BodiesOfInfluence.Add(go.gameObject);
+    }
+
+    void OnTriggerExit(Collider go)
+    {
+        //Debug.Log("TriggerExit Hit");
+        BodiesOfInfluence.Remove(go.gameObject);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += GravityPull();
+    }
+
+    private Vector3 GravityPull()
+    {
+        Vector3 t = Vector3.zero;
+
+        foreach(GameObject g in BodiesOfInfluence)
+        {
+            t.x += (2 * Mathf.PI * rangeOfInfluence) / speed;
+            t.z += (2 * Mathf.PI * rangeOfInfluence) / speed;
+        }
+
+        return t;
+    }
+}
---
+++ IDGA GameJam/Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private float rangeOfInfluence = 0.5f;
+    [SerializeField]
+    private float galavtitionalPull = 0;
+
+    private List<GameObject> BodiesOfInfluence;
+
+}
