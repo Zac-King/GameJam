3cf2edf7b42454813430f87ac8ca3b72db503faa
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-11-13 23:22:03 -0600

    File cleaning

--- IDGA GameJam/Assets/Scripts/CharacterController.cs
+++ IDGA GameJam/Assets/Scripts/CharacterController.cs
@@ -5,12 +5,14 @@ public class CharacterController : MonoBehaviour
 {
 	GameObject field;
 	public GameObject gravityField;
-
+	public GameObject source;
 	public float G_Force;
-	
+
+	public float speed;
+
 	public float gravityAcceleration;
 
-	void Gravity()
+	void GravityTool()
 	{
 		if(Input.GetMouseButton(0))
 		{
@@ -31,7 +33,7 @@ public class CharacterController : MonoBehaviour
 		{
 			G_Force -= Time.deltaTime * gravityAcceleration;
 		}
-		
+
 		else if(G_Force <= -.2)
 		{
 			G_Force += Time.deltaTime * gravityAcceleration;
@@ -43,10 +45,16 @@ public class CharacterController : MonoBehaviour
 		}
 	}
 
+	void OnTriggerEnter(Collider a)
+	{
+
+	}
+
 	// Use this for initialization
 	void Start () 
 	{
 		field = Instantiate(gravityField,transform.position,Quaternion.identity) as GameObject;
+		source = GameObject.FindWithTag("Player");
 	}
 
 	void GField()
@@ -67,7 +75,8 @@ public class CharacterController : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		Gravity();
+		GravityTool();
 		GField();
+		Movement();
 	}
 }
