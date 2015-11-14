78992d722453ed8af701f39de6f6542cb758ae06
Eric <EricMouledoux@Gmail.com>
2015-11-14 03:19:40 -0600

    UI
    How To menu open on click and stays open until mouse moves off. Requires
    more work.

---
+++ IDGA GameJam/Assets/Scripts/GrowthGage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowthGage : MonoBehaviour
+{
+	void Start ()
+	{
+	
+	}
+
+}
--- IDGA GameJam/Assets/Scripts/UI_Actions.cs
+++ IDGA GameJam/Assets/Scripts/UI_Actions.cs
@@ -4,18 +4,22 @@ using System.Collections;
 
 public class UI_Actions : MonoBehaviour
 {
-	
-	virtual protected void SetTimeScale(float a_scale)
+	virtual public void EnableDisable(GameObject other)
+	{
+		other.SetActive(!other.activeSelf);
+	}
+
+	virtual public void SetTimeScale(float a_scale)
 	{
 		Time.timeScale = a_scale;
 	}
 
-	virtual protected void ChangeScene(string a_scene)
+	virtual public void ChangeScene(string a_scene)
 	{
 		Application.LoadLevel(a_scene);
 	}
 
-	virtual protected void ExitApplication()
+	virtual public void ExitApplication()
 	{
 		Application.Quit();
 	}
