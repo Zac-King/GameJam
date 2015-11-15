2a0b2eccbffe3b086a6a07df43a2ca4271e1b98b
Eric <EricMouledoux@Gmail.com>
2015-11-15 00:19:32 -0600

    UI

--- IDGA GameJam/Assets/Scripts/Stats.cs
+++ IDGA GameJam/Assets/Scripts/Stats.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System;
 
@@ -61,10 +61,10 @@ public class Stats : MonoBehaviour
 		_fsm.Transition(_fsm.state, CSTATES.e_Shrinking);
 		minEnergy = 0;
 		currentEnergy = maxEnergy / 2;
-		if(FindObjectOfType<GrowthGauge>().gameObject)
+		if(FindObjectOfType<GrowthGage>().gameObject)
 		{
-			GameObject meter = FindObjectOfType<GrowthGauge>().gameObject;
-			meter.GetComponent<GrowthGauge>().energy = currentEnergy;
+			GameObject meter = FindObjectOfType<GrowthGage>().gameObject;
+			meter.GetComponent<GrowthGage>().energy = currentEnergy;
 		}
 		StartCoroutine(Shrinking());
 	}
@@ -120,8 +120,8 @@ public class Stats : MonoBehaviour
 	void Update () 
 	{
 		transform.localScale = new Vector3(currentEnergy, currentEnergy, currentEnergy);
-		GameObject meter = FindObjectOfType<GrowthGauge>().gameObject;
-		meter.GetComponent<GrowthGauge>().energy = currentEnergy;
+		GameObject meter = FindObjectOfType<GrowthGage>().gameObject;
+		meter.GetComponent<GrowthGage>().energy = currentEnergy;
 		if(_fsm.state == CSTATES.e_Dead)
 		{
 			StopAllCoroutines();
