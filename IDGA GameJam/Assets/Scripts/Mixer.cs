51c230d2eb17e8c18cd7c950ccd882fc31b0d3ab
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-11-15 00:22:07 -0600

    Sir Mix A Lot

---
+++ IDGA GameJam/Assets/Scripts/Mixer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Mixer : MonoBehaviour 
+{
+	public List<AudioSource>Tracks = new List<AudioSource>();
+	public Text trackName;
+	public int CurrentTrack = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		Tracks.AddRange(GetComponentsInChildren<AudioSource>());
+	}
+
+	public void SkipTrack()
+	{
+		Tracks[CurrentTrack].Stop();
+		if(CurrentTrack + 1 != Tracks.Count)
+		{
+			CurrentTrack++;
+			Play();
+		}
+	}
+
+	public void PrevTrack()
+	{
+		Tracks[CurrentTrack].Stop();
+		if(CurrentTrack - 1 <= 0)
+		{
+			CurrentTrack--;
+			Play();
+		}
+	}
+
+	public void Play()
+	{
+		Tracks[CurrentTrack].Play();
+		trackName.text = Tracks[CurrentTrack].clip.name;
+	}
+
+	public void Pause()
+	{
+		Tracks[CurrentTrack].Pause();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
