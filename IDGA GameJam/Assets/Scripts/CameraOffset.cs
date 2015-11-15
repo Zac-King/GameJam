ce73e19feca986f0d9ba8ea025294e326c936bde
Zac-King <Zacharyking18@yahoo.com>
2015-11-15 14:05:23 -0600

    CameraOffset works

---
+++ IDGA GameJam/Assets/Scripts/CameraOffset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOffset : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = (player.transform.position + offset) * player.transform.localScale.x;
+    }
+}
