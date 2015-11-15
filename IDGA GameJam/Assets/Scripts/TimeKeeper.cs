7074f8babb90630cad7f7842f7cdbd4b4f936c76
Eric <EricMouledoux@Gmail.com>
2015-11-15 01:27:05 -0600

    Time
    Static class to managing time scale

---
+++ IDGA GameJam/Assets/Scripts/TimeKeeper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeKeeper
+{
+	void Awakes ()
+	{
+		SetTimeScale(0);
+	}
+
+	static public void SetTimeScale(float a_value)
+	{
+		Time.timeScale = a_value;
+		globalScale = a_value;
+	}
+
+	static public float globalScale;
+}
