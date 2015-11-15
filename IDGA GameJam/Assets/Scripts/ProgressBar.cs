c2026b33fb99de7dc55535dac5900ecfee51a33b
Eric <EricMouledoux@Gmail.com>
2015-11-14 21:14:27 -0600

    SO MUCH!!!

---
+++ IDGA GameJam/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ProgressBar : MonoBehaviour
+{
+	void Start()
+	{
+		max = transform.localScale.x;
+		transform.localScale = new Vector3(0, transform.localScale.y, transform.localScale.z);
+
+		time = time == 0 ? 1 : time;
+	}
+
+	void Update ()
+	{
+		if(transform.localScale.x <= max)
+		{
+			transform.localScale += new Vector3(Time.deltaTime, 0, 0) / time;
+		}
+	}
+
+	float max;
+	public float time;
+}
